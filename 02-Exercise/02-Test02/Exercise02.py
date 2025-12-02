8a14586ba2710c8243f2540aa19a68531c904144
theflycity <1184668285@qq.com>
2025-12-02 16:57:47 +0800

    列表，元组，字符串

--- 02-Exercise/02-Test02/Exercise02.py
+++ 02-Exercise/02-Test02/Exercise02.py
@@ -1,23 +1,25 @@
 from ListNode import ListNode
+
+
 class Solution(object):
-   def addTwoNumbers(self, l1, l2):
-       """
+    def addTwoNumbers(self, l1, l2):
+        """
        :type l1: Optional[ListNode]
        :type l2: Optional[ListNode]
        :rtype: Optional[ListNode]
        """
-       head = ListNode()
-       tail = head
-       arr = 0 #进位
-       while l1 or l2 or arr:
-           if l1:
-               arr += l1.val
-               l1 = l1.next
-           if l2:
-               arr +=l2.val
-               l2 = l2.next
-           sub_node = ListNode(arr%10)
-           arr //=10
-           tail.next = sub_node
-           tail = sub_node
-       return head.next
+        head = ListNode()
+        tail = head
+        arr = 0  #进位
+        while l1 or l2 or arr:
+            if l1:
+                arr += l1.val
+                l1 = l1.next
+            if l2:
+                arr += l2.val
+                l2 = l2.next
+            sub_node = ListNode(arr % 10)
+            arr //= 10
+            tail.next = sub_node
+            tail = sub_node
+        return head.next
